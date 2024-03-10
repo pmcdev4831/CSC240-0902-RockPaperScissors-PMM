7da6a7b6a6f6848c8d0c68f7b53ee734a62fbbc7
mcdevittp <mcdevittp1@mymail.nku.edu>
2024-03-10 19:03:39 -0400

    completed coding

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace CSC240_0902_RockPaperScissors_PMM
 {
     public partial class Form1 : Form
@@ -6,25 +8,106 @@ namespace CSC240_0902_RockPaperScissors_PMM
         {
             InitializeComponent();
         }
+        int player_wins = 0, computer_wins = 0;
 
+        ArrayList list = new ArrayList();
+
+        Random random = new Random();
+
+        Bitmap rock = Properties.Resources.rock;
+        Bitmap paper = Properties.Resources.paper;
+        Bitmap scissors = Properties.Resources.scissors;
+        Bitmap random_picture;
         private void Form1_Load(object sender, EventArgs e)
         {
+            UxRock.Image = Properties.Resources.rock;
+            UxPaper.Image = Properties.Resources.paper;
+            UxScissor.Image = Properties.Resources.scissors;
 
+            list.Add(rock);
+            list.Add(paper);
+            list.Add(scissors);
         }
+
+
 
         private void UxRock_Click(object sender, EventArgs e)
         {
+            UxPlayerPictureBox.Image = rock;
+            random_picture = (Bitmap)list[random.Next(list.Count)];
 
+            UxComputerPictureBox.Image = random_picture;
+
+            getWinner(rock, random_picture);
+            
+           
         }
 
         private void UxPaper_Click(object sender, EventArgs e)
         {
+            UxPlayerPictureBox.Image = paper;
+            random_picture = (Bitmap)list[random.Next(list.Count)];
 
+            UxComputerPictureBox.Image = random_picture;
+
+            getWinner(paper, random_picture);
         }
 
         private void UxScissor_Click(object sender, EventArgs e)
         {
+            UxPlayerPictureBox.Image = scissors;
+            random_picture = (Bitmap)list[random.Next(list.Count)];
 
+            UxComputerPictureBox.Image = random_picture;
+
+            getWinner(scissors, random_picture);
+        }
+
+        public void getWinner(Bitmap player, Bitmap computer)
+        {
+            if (player.Equals(computer)) 
+            {
+            
+            }
+            else if (player.Equals(rock)) 
+            {
+                if(computer.Equals(scissors)) 
+                {
+                    player_wins++;
+                    UxPlayerScore.Text = player_wins.ToString();
+                }
+                else
+                {
+                    computer_wins++;
+                    UxComputerScore.Text = computer_wins.ToString();
+                }
+            }
+            else if (player.Equals(scissors))
+            {
+                if (computer.Equals(rock))
+                {
+                    player_wins++;
+                    UxPlayerScore.Text = player_wins.ToString();
+                }
+                else 
+                {
+                    computer_wins++;
+                    UxComputerScore.Text = computer_wins.ToString();
+                }
+            }
+            else if (player.Equals(paper))
+            {
+                if(computer.Equals(scissors))
+                {
+                    player_wins++;
+                    UxPlayerScore.Text = player_wins.ToString();
+                }
+                else
+                {
+                    computer_wins++;
+                    UxComputerScore.Text = computer_wins.ToString();
+                }
+            }
         }
     }
 }
